712fb8610e9088ce7ad96ac5d0a4d238fc65a96b
Duncan Murray <djmurray@acutesoftware.com.au>
2015-06-15 21:53:34 +0930

    test FunctionGenerator and calculations

--- tests/test_generate.py
+++ tests/test_generate.py
@@ -1,6 +1,7 @@
 # test_generate.py
 
 import unittest
+import random
 import rawdata.generate
 
 class TestGenerate(unittest.TestCase):
@@ -49,7 +50,60 @@ class TestGenerate(unittest.TestCase):
         self.assertEqual('Blue' in tbl.get_column(1), True) 
         self.assertEqual('Green' in tbl.get_column(1), True) 
         self.assertEqual('Orange' in tbl.get_column(1), True) 
-       
-    
+
+
+    def test_06_function_generator(self):
+        rng_mult = [-9999,9999]
+        rng_expt = [0,999]
+        #rng = [-2,9]
+        f1 = rawdata.generate.FunctionGenerator(rng_mult, rng_expt, num_terms=3)
+        self.assertEqual('a' in f1.equation, True) 
+        self.assertEqual('b' in f1.equation, True) 
+        self.assertEqual('c' in f1.equation, True) 
+        
+        self.assertEqual(f1.mult[0] >= rng_mult[0], True) 
+        self.assertEqual(f1.mult[0] < rng_mult[1]+1, True) 
+        
+        self.assertEqual(f1.expt[0] >= rng_expt[0], True) 
+        self.assertEqual(f1.expt[0] < rng_expt[1]+1, True) 
+        
+        #print(f1)
+    def test_07_function_answer(self):
+        """
+        Set the max and min ranges for randomisation the same
+        to generate a fixed equation to test against known answers
+        Equation   : 2a^2+2b^2+2c^2
+        Parameters : a=1,b=2,c=3
+        answer     : 10        
+        """
+        rng_mult = [2,2]
+        rng_expt = [2,2]
+        f3 = rawdata.generate.FunctionGenerator(rng_mult, rng_expt, num_terms=3)
+        self.assertEqual(f3.equation, '2a^2+2b^2+2c^2')
+        c1 = rawdata.generate.FunctionCalculator(f3, [1,2,3])
+        self.assertEqual(c1.answer, '10')
+        c2 = rawdata.generate.FunctionCalculator(f3, [5,-4,33])
+        self.assertEqual(c2.answer, '82')
+        c3 = rawdata.generate.FunctionCalculator(f3, [1.72,2.32,5.115])
+        self.assertEqual(c3.answer, '16.6816')
+        c4 = rawdata.generate.FunctionCalculator(f3, [-2,-66,4543.1118])
+        self.assertEqual(c4.answer, '8720')
+        #print(c3)
+        
+        
+    def test_08_function_calculator(self):
+        rng_mult = [-9,99]
+        rng_expt = [0,9]
+        f2 = rawdata.generate.FunctionGenerator(rng_mult, rng_expt, num_terms=3)
+        
+        
+        
+        for i in range(5):
+            n = rawdata.generate.NumberGenerator()
+            params = [n.random_int(i,i+5), n.random_int(i,i+5), n.random_int(i,i+5)]
+            c = rawdata.generate.FunctionCalculator(f2, params, i)
+            #print(c)
+ 
+        
 if __name__ == '__main__':
     unittest.main()
