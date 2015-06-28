476978a96e7bc2caa3bb086fe0c390ae3c11adeb
Duncan Murray <djmurray@acutesoftware.com.au>
2015-06-28 15:55:47 +0930

    cleaned folder structure - stuff to scripts, readme

---
+++ scripts/collect_example.py
@@ -0,0 +1,23 @@
+# collect_example.py
+
+def main():
+    print('collect_example.py')
+    print('Collect_* scripts are used to download large')
+    print('datasets and selectively extract lists into')
+
+    print('you local \rawdata\data\* subfolders')
+
+    print('[TODO] Modify this script to choose how to process')
+    print('[TODO] 1. download ZIP')
+    print('[TODO] 2. extract ontology raw data files')
+    print('[TODO] 3. update lists in \data folder')
+
+
+    print('Done')
+
+
+
+
+
+    
+main()
---
+++ scripts/install.py
@@ -0,0 +1,15 @@
+# install.py
+
+def main():
+    print('install.py')
+    print('Script (empty for now) to setup local data')
+    print('folders, connections to databases, etc.\n')
+
+    print('[TODO] Setup Local folder structure...')
+
+    print('[TODO] Link to databases...')
+
+
+    print('Done')
+ 
+main()
