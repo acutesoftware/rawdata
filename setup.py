929c5537d79caa965984f9eae52895083a38a3fd
Duncan Murray <djmurray@acutesoftware.com.au>
2015-07-01 00:14:21 +0930

    setup.py updated for upcoming release 0.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='rawdata',
-    version='0.0.9',
+    version='0.1.0',
     author='Duncan Murray',
     author_email='djmurray@acutesoftware.com.au',
     packages=['rawdata'],
