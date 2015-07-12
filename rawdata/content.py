5b40fe4a394334ef97b53cc97c518757f7bac533
Duncan Murray <djmurray@acutesoftware.com.au>
2015-07-12 20:35:06 +0930

    renaming and merging old samples and content with datafiles

--- rawdata/content.py
+++ rawdata/content.py
@@ -13,61 +13,61 @@ sample_xtn = '*.sample'
 
 def TEST():
     s = Samples()
-    print(s.get_list())
-    col1 = s.get_collist_by_name(data_fldr + os.sep + 'games' + os.sep + 'skills.csv', 'type' )
-    print(col1)
-    
-    # get list of countries from copper production
-    fname = data_fldr + os.sep + 'finance' + os.sep + 'mining_copper_rent.csv'
-    country_names_copper = s.get_collist_by_name(fname, 'Country Name')  # country code, Country Name
-    print(country_names_copper)
-    # [{'CHILE', 'AUSTRALIA', 'UNITED KINGDOM', 'Cuba', 'SOUTH AFRICA', 'INDONESIA', 
-    #   'GUATEMALA', 'PHILIPPINES', 'NORWAY', 'ZIMBABWE', 'AUSTRIA', 'CYPRUS', 'CHINA', ...
+    #print(s.get_list())  # list of data files [fullname, shortname]
+    #col1 = s.get_collist_by_name(data_fldr + os.sep + 'games' + os.sep + 'skills.csv', 'type' )
+    # [{'build', 'buff', 'gather', 'type', 'heal', 'change', 'attack', 'info'}]
 
-    # get food lists
-    food = s.get_collist_by_name(data_fldr + os.sep + 'food' + os.sep + 'food_desc.csv', 'Long_Desc')
-    for f in food[0]:
-        print(f)
-    
-    
     f = s.get_sample_by_name('finance_transaction')
     print(f)
-    print(s)
     
-class Samples(object):
+    
+    d = DataFiles()
+    print('Datafiles', d)
+    print('columns')
+    for c in d.columns:
+        print(c[0][len(data_fldr) + 1:] + '.' + c[2] + '.' + c[3])
+    #for i in s.samples:
+    #    print(i)
+    
+    
+class DataFiles(object):
     """
-    read samples from data subfolder to get lists
+    read data files from data subfolder to get lists
+        columns = [folder, filename, short_name, column_name]
+            games.skills.area
+            games.skills.description
+            food.food_desc.NDB_No
+            food.food_desc.Refuse
+            food.food_desc.SciName
+            food.food_desc.N_Factor
+            food.garden_produce.method
+            food.garden_produce.type
+            food.garden_produce.name
+            finance.mining_copper_rent.Country Code
+            finance.mining_copper_rent.Country Name
+            finance.mining_copper_rent.1978
+            finance.mining_copper_rent.1979
+            world.country.2-alpha code
+            world.country.WB-2 code
+            world.country.Table Name
+            world.country.Short Name        
     """
     def __init__(self):
         self.filelist = []
-        self.sample_list = []
-        self.samples = []
-        # data files
+        self.columns = []
         for root, _, files in os.walk(data_fldr):
             for f in files:
                 self.filelist.append([root + os.sep + f, root,f])
-
-        # samples from samples folder        
-        self.sample_list = []  # filelist = [shortname, fullpath]
-        self.samples = []       # list of Sample objects
-        for root, _, files in os.walk(rootPath):
-            for basename in files:
-                if fnmatch.fnmatch(basename, sample_xtn):
-                    filename = os.path.join(root, basename)
-                    self.sample_list.append([basename, filename])
-                    self.samples.append(Sample(filename))
-                
+                cols = self.get_all_columns(root + os.sep + f)
+                for c in cols:
+                    self.columns.append([root,f, f[:-4], c])
+                    
                 
     def __str__(self):
         txt = 'Data files read from :\n'
         txt += data_fldr + '\n'
         for row in self.filelist:
             txt += row[0] + '\n'
-            
-            
-        txt += '\nList of available sample definitions\n'
-        for d in self.sample_list:
-            txt += d[0]  #.ljust(30) + d[1] + '\n'            
         return txt    
     
     def get_list(self, names_only=False):
@@ -102,7 +102,41 @@ class Samples(object):
     
                 res.append(cols[ndx].strip('"'))
         return [set(res)]            
-   
+
+    def get_all_columns(self, filename):
+        with open(filename) as f:
+            ndx = 0
+            res = []
+            for num, line in enumerate(f):
+                #print('line',line, 'num',num)
+                cols = line.split(',')
+                if num == 0:
+                    for col in cols:
+                        res.append(col.strip('"').strip('\n'))
+        return res       
+
+        
+class Samples(object):
+    """
+    class to manage the list of sample files in /samples folder
+    """
+    def __init__(self):
+        self.sample_list = []  # filelist = [shortname, fullpath]
+        self.samples = []      # list of Sample objects
+        for root, _, files in os.walk(rootPath):
+            for basename in files:
+                if fnmatch.fnmatch(basename, sample_xtn):
+                    filename = os.path.join(root, basename)
+                    self.sample_list.append([basename, filename])
+                    self.samples.append(Sample(filename))
+                
+                
+    def __str__(self):
+        txt = 'List of available sample definitions\n'
+        for d in self.sample_list:
+            txt += d[0]  #.ljust(30) + d[1] + '\n'            
+        return txt    
+    
     def get_sample_by_name(self, txt):
         """
         returns the first Sample that matches txt
@@ -121,6 +155,8 @@ class Samples(object):
             res += l + '\n'
         return res
 
+        
+        
 class Sample(object):
     """
     class to manage a single sample, read from 
@@ -131,6 +167,7 @@ class Sample(object):
         reads the .sample file passed and loads 
         to class.
         """
+        #print('INIT SAMPLE')
         self.fullname = fullname
         self.cols = []
         self.lists = []
@@ -143,6 +180,7 @@ class Sample(object):
                      
     
     def __str__(self):
+        #print('PRINT SAMPLE')
         res = 'Sample File = ' + self.fullname + '\n'
         for num, c in enumerate(self.cols):
             res += 'Column#' + str(num) + ' = ' + c + '\n'
--- tests/test_content.py
+++ tests/test_content.py
@@ -5,32 +5,55 @@ import unittest
 import os
 import sys
 
-root_fldr = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+root_fldr = os.path.abspath(os.path.join(os.path.dirname(__file__), '..')) + os.sep + 'rawdata'
 test_fldr = os.path.dirname(__file__) + os.sep + 'test_results'
 sys.path.insert(1, root_fldr)
 
-import rawdata.generate
-import rawdata.content
+import generate
+import content
 
 class TestContent(unittest.TestCase):
     def test_01_check_sample_datafiles(self):
-        s = rawdata.content.Samples()
+        s = content.DataFiles()
         all_files = s.get_list()
         #print(len(all_files))
         self.assertEqual(len(all_files) > 12, True)  # check for at least 12 data files
         #self.assertEqual(all_files[0][2],'countries.csv')
 
     def test_02_get_collist(self):
-        s = rawdata.content.Samples()
-        data_file = rawdata.content.data_fldr + os.sep + 'games' + os.sep + 'skills.csv'
+        s = content.DataFiles()
+        data_file = content.data_fldr + os.sep + 'games' + os.sep + 'skills.csv'
         col1 = s.get_collist_by_name(data_file, 'type' )
         self.assertEqual(col1, [{'buff', 'change', 'build', 'type', 'info', 'gather', 'attack', 'heal'}])
         
     def test_03_read_list(self):
-        s = rawdata.content.Samples()
-        fname = rawdata.content.data_fldr + os.sep + 'food' + os.sep + 'food_desc.csv'
+        s = content.DataFiles()
+        fname = content.data_fldr + os.sep + 'food' + os.sep + 'food_desc.csv'
         food_list = s.get_collist_by_name(fname, 'Long_Desc')
         self.assertEqual(len(food_list[0]), 1114)
+    
+    def test_04_read_data_copper(self):
+        # get list of countries from copper production
+        s = content.DataFiles()
+        fname = content.data_fldr + os.sep + 'finance' + os.sep + 'mining_copper_rent.csv'
+        country_names_copper = s.get_collist_by_name(fname, 'Country Name')  # country code, Country Name
+        self.assertEqual(len(country_names_copper[0]), 68)  # 68 countries mine copper
+        # print(country_names_copper)
+        # [{'CHILE', 'AUSTRALIA', 'UNITED KINGDOM', 'Cuba', 'SOUTH AFRICA', 'INDONESIA', 
+        #   'GUATEMALA', 'PHILIPPINES', 'NORWAY', 'ZIMBABWE', 'AUSTRIA', 'CYPRUS', 'CHINA', ...
+    
+    def test_05_list_data_file_columns(self):
+        """
+        d = DataFiles()
+        print('Datafiles', d)
+        print('columns')
+        for c in d.columns:
+            print(c[0][len(data_fldr) + 1:] + '.' + c[2] + '.' + c[3])
+        """
+        d = content.DataFiles()
+        self.assertEqual(len(d.columns) > 120, True)
+        
+        
     
 if __name__ == '__main__':
     unittest.main()
