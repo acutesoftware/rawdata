69c76a217efe530c410f2962e04e0db6644e7c97
Duncan Murray <djmurray@acutesoftware.com.au>
2015-07-20 21:36:01 +0930

    run_tests.py doesnt do if main check - just runs all to increase coverage

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -1,17 +1,13 @@
 # run_tests.py
 
-import unittest as unittest
- 
-if __name__ == "__main__":
-    """
-    T:\\user\\dev\\src\\python\\rawdata\\tests>run_tests.py
-    11
-    ............
-    ----------------------------------------------------------------------
-    Ran 12 tests in 0.211s
+# T:\\user\\dev\\src\\python\\rawdata\\tests>run_tests.py
+# 11
+# ............
+# ----------------------------------------------------------------------
+# Ran 12 tests in 0.211s
+# OK
 
-    OK
-    """
-    all_tests = unittest.TestLoader().discover('.', pattern='test*.py')
-    unittest.TextTestRunner().run(all_tests)  
-    
+import unittest as unittest
+
+all_tests = unittest.TestLoader().discover('.', pattern='test*.py')
+unittest.TextTestRunner().run(all_tests)  
