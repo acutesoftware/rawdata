11d938beb10d16edac97641ba7cab1e850c119fa
Duncan Murray <djmurray@acutesoftware.com.au>
2016-03-21 23:24:56 +1030

    script to lookup location data on Google map API

---
+++ scripts/gather/location_data.py
@@ -0,0 +1,57 @@
+# location_data.py
+
+import requests
+import pprint
+import os
+
+lst = ['Glenelg Jetty', 'Sydney Opera House', 'Uluru', 'New York,Central Park','Zimbabwe,Victoria Falls','1 King William St, Adelaide, Australia'] 
+
+
+
+base_url = 'https://maps.googleapis.com/maps/api/geocode/json?'
+
+raw_json_file = 'json_raw.txt'
+if os.path.exists(raw_json_file):
+    os.remove(raw_json_file)
+
+def main():
+    res = '"search_term","result_num","gps_lat","gps_lng","formatted_address",\n'
+    for name in lst:
+       res += lookup_address(name)
+    with open('address.csv', 'w') as f:
+        f.write(res)
+    print('Done')
+
+def lookup_address(nme):
+    """
+    use the Google map API to get address details
+    """
+    csv_line = ''        
+    response = requests.get(base_url + 'address=' + nme)
+
+    resp_json_payload = response.json()
+    with open(raw_json_file, 'a') as f_raw:
+        pprint.pprint(resp_json_payload, stream=f_raw)
+        
+    for result_num,result in enumerate(resp_json_payload['results']):
+        csv_line += '"' + nme + '",'        
+        csv_line += '"' + str(result_num+1) + '",'        
+        for add in result['address_components']:
+            if 'type' in add:
+                if 'premise' in add['type']:
+                    csv_line += '"' + add['short_name'] + '",'
+                else:
+                    csv_line += '",",'
+                    
+                if 'country' in add['type']:
+                    csv_line += '"' + add['short_name'] + '",'
+                else:
+                    csv_line += '",",'
+                    
+        csv_line += '"' + str(result['geometry']['location']['lat']) + '",'
+        csv_line += '"' + str(result['geometry']['location']['lng']) + '",'
+        csv_line += '"' + result['formatted_address'] + '",'
+        csv_line += '\n'            
+    return csv_line
+    
+main()
