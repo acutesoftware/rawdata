9a64b358c5ce858fd75de85425d8e9069d1303b4
Duncan Murray <djmurray@acutesoftware.com.au>
2016-04-01 00:32:41 +1030

    creates notes.csv based on TableGenerator(STRING)

--- scripts/create_pim_data.py
+++ scripts/create_pim_data.py
@@ -29,27 +29,10 @@ t_task.save_table(op_fldr + 'tasks.csv')
 
 # Contacts
 lbl_contact = ['Year_met', 'Customer_id', 'Age', 'First Name', 'Last Name', 'Country', 'Amount']
-tpe_contact = ['DATE', 'STRING', 'INT', 'PEOPLE', 'PEOPLE', 'PLACE', 'CURRENCY']
+tpe_contact = ['YEAR', 'STRING', 'INT', 'PEOPLE', 'PEOPLE', 'PLACE', 'CURRENCY']
 t_contact = generate.TableGenerator(50, tpe_contact, lbl_contact)
 t_contact.save_table(op_fldr + 'contacts.csv')
 
-# test for new stuff
-sample_dict = {'name' :'test_dict_by_weekday', 'scale':'day_of_week', 
-               'trend':{'Mon':0.5, 'Tue':0.5, 'Wed':0.1, 'Thu':0.1, 'Fri':0.5}}
-
-#sample_data = [['2016-03-01', 20], ['2016-04-22', 50]]
-dates = ['2016-03-' + str(d) for d in range(1,31)]
-sample_data = [[d, ndx] for ndx,d in enumerate(dates)]
-print('BEFORE = ', sample_data)
-t = events.TrendGenerator(sample_dict)
-t.create_time_series(sample_data, 0, 1) 
-print('AFTER = ', sample_data)
-
-
-"""d = dict([(num + 335, round(0.14 + num*n,3)) 
-                     for num, n in enumerate(
-                      [0.14/25 for n in range(1, 25)])])
-                      
-                      
-print(d)                      
-"""
+# Notes
+t_notes = generate.TableGenerator(10, ['DATE','WORD','TEXT'], ['Date','Tags','Note Contents'])
+t_notes.save_table(op_fldr + 'notes.csv')
