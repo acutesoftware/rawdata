b133e5e5b5bd17aafb4394025e2c646f230cb3fe
Duncan Murray <djmurray@acutesoftware.com.au>
2016-05-06 00:08:23 +0930

    sales example uses products and customers created separately

--- scripts/sales_example.py
+++ scripts/sales_example.py
@@ -9,17 +9,23 @@ root_fldr = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'rawda
 sys.path.insert(1, root_fldr)
 
 import generate
-import errors
+import content
 
 def main():
     """
     generate a sales example with tables for customers,
     sales, products
     """
-    date_list = generate.get_list_dates(2009, 2016)
-    product_list = ['cabbage', 'online game', 'pencil', 'Milk', 'used car', 'plane ticket']
-    tbl_sales = generate.TableGenerator(25, [date_list, 'PEOPLE', product_list, 'CURRENCY'], ['Date of sale', 'CustomerName', 'Product', 'Amount'])
-    tbl_sales.save_table('sales.csv')
+    s = content.DataFiles()
     
-
+    date_list = generate.get_list_dates(2016, 2016, 500)
+    prod_list = list(s.get_collist_by_name(os.path.join(content.data_fldr,'food','garden_produce.csv'), 'name')[0])
+    
+    tbl_cust = generate.TableGenerator(8, ['STRING','PEOPLE', 'PEOPLE', 'PLACE'], ['Customer ID', 'First Name', 'Surname', 'Country'])
+    tbl_cust.save_table('customers.csv')
+    cust_list = list(s.get_collist_by_name('customers.csv', 'Customer ID')[0])
+    
+    tbl_sales = generate.TableGenerator(25, [date_list, cust_list, prod_list, 'CURRENCY'], ['Date of sale', 'Customer ID', 'Product', 'Amount'])
+    tbl_sales.save_table('sales.csv')
+ 
 main()
