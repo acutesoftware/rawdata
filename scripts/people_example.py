2f503fa867ef35561cddfae53d75037baf7b07d9
Duncan Murray <djmurray@gmail.com>
2017-05-25 22:36:31 +0930

    working on v2 of random person generator using LISTS and COLS from generator

--- scripts/people_example.py
+++ scripts/people_example.py
@@ -20,6 +20,10 @@ def main():
     """
     persons = make_random_set_of_persons(3)
     pprint.pprint(persons)
+   
+    # V2 - not working yet
+    #workers = make_random_set_of_workers(5)
+    #pprint.pprint(workers)
     
 def make_random_set_of_persons(num_persons=5):    
     s = content.Samples(root_path)
@@ -29,11 +33,52 @@ def make_random_set_of_persons(num_persons=5):
         persons.append(random_person(person_generator))
     return persons
     
+   
+def make_random_set_of_workers(num_persons=5):    
+    s = content.Samples(root_path)
+    person_generator = s.get_sample_by_name('person_worker')
+    persons = []
+    print('person_generator = ', person_generator)
+    for n in range(num_persons):
+        persons.append(random_person_v2(person_generator))
+    return persons
+    
 def random_person(person_generator):
+    """ 
+    original function 
+    """
     person = {}
     for l in person_generator.lists:
         for k,v in l.items():
             person[k] = random.choice(v)
+    return person
+    
+def random_person_v2(person_generator):
+    """ 
+    this should use the LISTS and the COLUMNS
+    which the original function isnt doing - not there yet
+    """
+    person = {}
+    custom_list = 'N'
+    
+    print('person_generator.col_labels = ', person_generator.col_labels)
+    print('person_generator.col_types = ', person_generator.col_types)
+    for num, l in enumerate(person_generator.col_labels):
+        custom_list = 'N'
+        if l in person_generator.lists:
+                custom_list = 'Y'
+    
+    
+        if custom_list == 'Y':
+            print('custom list = ', person_generator.lists)
+        if person_generator.col_types == 'NAME':
+            print('getting name')
+        elif person_generator.col_types == 'WORD':
+            print('getting word')
+        elif person_generator.col_types == 'PLACE':
+            print('getting place')
+        else:   # it is a list
+            person[l] = random.choice(person_generator.col_types)
     return person
     
     
