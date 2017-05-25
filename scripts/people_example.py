f7d1e7fb96272d20cabd77ded9d951e7ddbc105e
Duncan Murray <djmurray@gmail.com>
2017-05-26 00:20:50 +0930

    working on custom samples with people_example

--- scripts/people_example.py
+++ scripts/people_example.py
@@ -18,12 +18,12 @@ def main():
     """
     generate a large number of people with random attributes
     """
-    persons = make_random_set_of_persons(3)
-    pprint.pprint(persons)
+    #persons = make_random_set_of_persons(3)
+    #pprint.pprint(persons)
    
     # V2 - not working yet
-    #workers = make_random_set_of_workers(5)
-    #pprint.pprint(workers)
+    workers = make_random_set_of_workers(5)
+    pprint.pprint(workers)
     
 def make_random_set_of_persons(num_persons=5):    
     s = content.Samples(root_path)
@@ -38,7 +38,11 @@ def make_random_set_of_workers(num_persons=5):
     s = content.Samples(root_path)
     person_generator = s.get_sample_by_name('person_worker')
     persons = []
-    print('person_generator = ', person_generator)
+    #print('person_generator = ', person_generator)
+    print('person_generator.col_labels = ', person_generator.col_labels)
+    print('person_generator.col_types = ', person_generator.col_types)
+    print('person_generator.col_source = ', person_generator.col_source)
+    print('person_generator.lists = ', person_generator.lists)
     for n in range(num_persons):
         persons.append(random_person_v2(person_generator))
     return persons
@@ -59,24 +63,24 @@ def random_person_v2(person_generator):
     which the original function isnt doing - not there yet
     """
     person = {}
-    custom_list = 'N'
+    custom_list = []
     
-    print('person_generator.col_labels = ', person_generator.col_labels)
-    print('person_generator.col_types = ', person_generator.col_types)
     for num, l in enumerate(person_generator.col_labels):
-        custom_list = 'N'
-        if l in person_generator.lists:
-                custom_list = 'Y'
-    
-    
-        if custom_list == 'Y':
-            print('custom list = ', person_generator.lists)
+        custom_list = []
+        for lst in person_generator.lists:
+            #print('lst = ', lst)
+            for k,v in lst.items():
+                if person_generator.col_source[num] == k:
+                    print('hello - using list values ', v)
+                    custom_list = v
+        if custom_list != []:
+            person[l] = random.choice(custom_list)
         if person_generator.col_types == 'NAME':
-            print('getting name')
+            person[l] = 'getting name'
         elif person_generator.col_types == 'WORD':
-            print('getting word')
+            person[l] = 'getting word'
         elif person_generator.col_types == 'PLACE':
-            print('getting place')
+            person[l] = 'getting place'
         else:   # it is a list
             person[l] = random.choice(person_generator.col_types)
     return person
