8e3215b86f151bcd00e487849dbd18cdef594de7
Duncan Murray <djmurray@gmail.com>
2017-06-01 20:15:24 +0930

    working out unique values to identify grain of table

--- scripts/identify_grain.py
+++ scripts/identify_grain.py
@@ -47,13 +47,30 @@ def get_counts_by_col(tbl):
     get counts by all permutations of a table
     """
     cnt = {}
+    col_vals = {}
+    #print('get_counts_by_col(tbl)  = ', tbl[0:5])
     for col_num, c in enumerate(tbl[0]):
-        cnt[col_num] = 0
-    for r in tbl:
+        col_vals[tbl[0][col_num]] = get_unique_vals(tbl, col_num)
+        cnt[col_num] = len(col_vals[tbl[0][col_num]])
+        
+    
+
+    print('col_vals = ', str(col_vals)[0:400])
+    
+    for r in tbl[1:]:
         for col_num, c in enumerate(r):
             cnt[col_num] += 1
         
     return cnt
     
+ 
+def get_unique_vals(tbl, col_num):
+    """
+    returns a set of unique values in col_num of tbl
+    """
+    vals = []
+    for r in tbl[1:]:
+        vals.append(r[col_num])
+    return list(set(vals))
  
 main()
