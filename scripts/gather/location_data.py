c1e392c1b8194869ce9814d36b3069b5ffb55e11
Duncan Murray <djmurray@gmail.com>
2017-08-25 00:01:19 +0930

    fixed paths for location collection script

--- scripts/gather/location_data.py
+++ scripts/gather/location_data.py
@@ -6,7 +6,10 @@ import requests
 import pprint
 import os
 
-fldr = os.path.join(os.getcwd(),'..','..','..','test')
+root_fldr = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
+
+fldr = os.path.join(root_fldr,'tests')
+
 #src_file = os.path.join(fldr,'list_names_to_lookup.txt')
 src_file = os.path.join(fldr,'list_names_to_lookup_CUT.txt')
 
@@ -16,24 +19,43 @@ if os.path.exists(src_file):
         for line in f:
             lst.append(line.strip('\n'))
 else:
-    lst = ['Glenelg Jetty', 'Sydney Opera House', 'Uluru', 'New York,Central Park','Zimbabwe,Victoria Falls','1 King William St, Adelaide, Australia'] 
+    lst = ['Glenelg Jetty', 
+           'Sydney Opera House', 
+           'Uluru', 
+           'New York,Central Park',
+           'Zimbabwe,Victoria Falls',
+           '1 King William St, Adelaide, Australia',
+           'Eiffel Tower',
+           'Grand Canyon National Park',
+           'Big Ben, London',
+           'Kentucky Fried Chicken, Melbourne Central',
+           'Mt Everest',
+           '143 E Houston St, New York',
+           'Ploshchad (Square)',
+           'ABC, Perth'] 
      
 
 
 base_url = 'https://maps.googleapis.com/maps/api/geocode/json?'
 
-raw_json_file = os.path.join(fldr,'json_raw.txt')
+#raw_json_file = os.path.join(fldr,'json_raw.txt')
+raw_json_file = os.path.join(os.getcwd(), 'json_raw.txt')
+address_file = os.path.join(os.getcwd(), 'address.csv')
+
+
 if os.path.exists(raw_json_file):
     os.remove(raw_json_file)
+if os.path.exists(address_file):
+    os.remove(address_file)
 
 def main():
-    res = '"search_term","result_num","gps_lat","gps_lng","formatted_address",\n'
+    res = '"search_term","result_num","gps_lat","gps_lng","formatted_address","country",\n'
     for name in lst:
         try:
             res += lookup_address(name)
         except Exception as ex:
             print('missing data for ', name)
-    with open(os.path.join(fldr,'address.csv'), 'w') as f:
+    with open(address_file, 'w') as f:
         f.write(res)
     print('Done')
 
@@ -75,6 +97,10 @@ def lookup_address(nme):
         csv_line += '"' + str(result['geometry']['location']['lat']) + '",'
         csv_line += '"' + str(result['geometry']['location']['lng']) + '",'
         csv_line += '"' + result['formatted_address'] + '",'
+        
+        add_details = result['formatted_address'].split(',')
+        csv_line += '"' + add_details[len(add_details)-1].strip(' ').upper() + '",'
+        
         csv_line += '\n'            
     return csv_line
     
